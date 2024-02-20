739a53d84d3bbe37493f70450beb1b0162775afa
huseyinkedik <159181717+huseyinkedik@users.noreply.github.com>
2024-02-20 21:30:29 +0300

    Düzenleme-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k;
+        int toplam = 0;
+
+        //Kullanıcıdan sayı girilmesi bekleniyor.
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı giriniz: ");
+            k = input.nextInt();
+
+            //Girilen sayının çift ve 4'ün katı olması koşulunu sağlayanlar toplanıyor
+
+            if ((k % 2 == 0) && (k % 4 == 0)) {
+                toplam += k;
+            }
+        } while (k % 2 == 0);
+        System.out.println("Toplam: " + toplam);
     }
 }
